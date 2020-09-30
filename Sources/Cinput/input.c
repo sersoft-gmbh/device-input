db769b79cf99203a93c441bcfe98c993aa0ca3b5
Florian Friedrich <ffried@me.com>
2020-09-30 20:21:44 +0200

    Fix compilation on older linux kernels

--- Sources/Cinput/input.c
+++ Sources/Cinput/input.c
@@ -1,9 +1,17 @@
 #include "input.h"
 
 inline int input_event_get_sec(struct input_event event) {
+#ifdef input_event_sec
     return event.input_event_sec;
+#else
+    return event.time.tv_sec
+#endif
 }
 
 inline int input_event_get_usec(struct input_event event) {
+#ifdef input_event_usec
     return event.input_event_usec;
+#else
+    return event.time.tv_usec
+#endif
 }
