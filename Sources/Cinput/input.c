80063607a7dcafb7e1b20e65493e4d365c644401
Florian Friedrich <ffried@me.com>
2020-09-30 20:23:12 +0200

    Add missing semicolons

--- Sources/Cinput/input.c
+++ Sources/Cinput/input.c
@@ -4,7 +4,7 @@ inline int input_event_get_sec(struct input_event event) {
 #ifdef input_event_sec
     return event.input_event_sec;
 #else
-    return event.time.tv_sec
+    return event.time.tv_sec;
 #endif
 }
 
@@ -12,6 +12,6 @@ inline int input_event_get_usec(struct input_event event) {
 #ifdef input_event_usec
     return event.input_event_usec;
 #else
-    return event.time.tv_usec
+    return event.time.tv_usec;
 #endif
 }
